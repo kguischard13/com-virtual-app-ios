5d06f3df939cdc4f5f9b61a2628b381771ed1a6d
Caf131 <christopher.forehand@gmail.com>
2014-04-12 12:43:56 -0400

    Changed Home View URL

--- ActivityView.php
+++ ActivityView.php
@@ -29,14 +29,14 @@
 				<div id="navigation">
 					<div id="nav-list">
 						<ul>
-							<li class="top-level"><a href="./HomeView.html" id="home">Home</a></li>
+							<li class="top-level"><a href="./HomeView.php" id="home">Home</a></li>
 							<li class="top-level"><a href="#" id="how" title="Take the tutorial if you are a newcomer or view help documentation.">How it works?</a></li>
 							<li class="top-level"><a href="./EmotionSelection.php" id="emotions" title="Pick your current emotion to view new activities.">Emotions</a></li>
-							<li class="top-level"><a href="./ActivityView.html" id="activities" title="View your recent, favorite, or trending activities.">Activities</a></li>
+							<li class="top-level"><a href="./ActivityView.php" id="activities" title="View your recent, favorite, or trending activities.">Activities</a></li>
 							<li class="top-level"><a href="#" id="account">My Account</a>
 								<ul id="sub-menu">
 									<li class="sub-level"><a href="./LoginView.php" id="login" title="Log in if you already have an account.">Login</a></li>
-									<li class="sub-level"><a href="./RegistrationView.html" id="new-user" title="New users, register here">Sign up</a></li>
+									<li class="sub-level"><a href="./RegistrationView.php" id="new-user" title="New users, register here">Sign up</a></li>
 									<li class="sub-level"><a href="#" id="forgot-password" title="Forgot your password?">Forgot my password</a></li>
 
 								</ul>
